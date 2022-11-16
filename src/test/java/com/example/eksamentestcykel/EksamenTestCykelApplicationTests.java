87846270d3f0910669e85ebbbad0f957479cc4b8
Violetta87 <athena_mewo87@hotmail.com>
2022-11-16 16:24:37 +0100

    new update

--- src/test/java/com/example/eksamentestcykel/EksamenTestCykelApplicationTests.java
+++ src/test/java/com/example/eksamentestcykel/EksamenTestCykelApplicationTests.java
@@ -6,8 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EksamenTestCykelApplicationTests {
 
-    /*@Test
-    void contextLoads() {
-    }
-*/
+    //two tests
+
+    //arrange
+
+    //act
+
+    //assert
 }
